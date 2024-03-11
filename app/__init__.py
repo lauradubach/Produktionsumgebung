bad786ff091ad074b57a455dada86456f4c0e5c2
langerbo <boris.langer@tbz.ch>
2024-03-11 13:35:40 +0100

    moved test_data creation to test folder

--- app/__init__.py
+++ app/__init__.py
@@ -2,23 +2,22 @@ from apiflask import APIFlask
 
 from config import Config
 from app.extensions import db
-from app.models.test_data import create_test_data
-
-
+from test.create_test_data import create_test_data
 
 
 def create_app(config_class=Config):
     app = APIFlask(__name__)
     app.config.from_object(config_class)
 
-    # Initialize Flask extensions here
+    # Flask Erweiterungen initialisieren
     db.init_app(app)
         
+    # Datenbanktabellen anlegen
     with app.app_context():
         db.create_all()
-        create_test_data()  ## remove this command when not testing
+        create_test_data()  ## diese Zeile auskommentieren, wenn man keine Testdaten braucht
 
-    # Register blueprints here
+    # Blueprints registrieren
     from app.students import bp as students_bp
     app.register_blueprint(students_bp, url_prefix='/students')
 
