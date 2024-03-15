23a785b9857b1bad40fdab259e0a19979cd3fde0
langerbo <boris.langer@tbz.ch>
2024-03-15 12:10:48 +0100

    added status_code checks in all tests

--- test/test_student.py
+++ test/test_student.py
@@ -3,10 +3,12 @@ import pytest
 
 def test_get_students(client):
     response = client.get("/students/")
+    assert response.status_code == 200
     assert response.json[4]['name'] == 'Mega Tron'
 
 def test_get_student(client):
     response = client.get("/students/2")
+    assert response.status_code == 200
     assert response.json["name"] == "Sam Sung"
 
 def test_post_student(client):
@@ -17,6 +19,7 @@ def test_post_student(client):
 
 def test_change_student(client):
     response = client.patch("/students/2", json={'level': 'AP'})
+    assert response.status_code == 200
     assert response.json['level'] == 'AP'
 
 def test_register_student(client):
@@ -29,4 +32,5 @@ def test_course_double_register(client):
 
 def test_get_student_courses(client):
     response = client.get("/students/5/courses")
+    assert response.status_code == 200
     assert response.json[0]['title'] == 'M347 Kubernetes'
